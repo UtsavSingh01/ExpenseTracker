bbe5fd793571853c07d3cc3e480e2b9617d2b554
Utsav Kumar <Utsavsingh611@gmail.com>
2024-06-22 17:53:24 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Dependency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2UFhhQlJBfVhdVHxLflFyVWZTel96cVxWESFaRnZdRl1lSXdTc0BnWnlXeXxX");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.Run();
